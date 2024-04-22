86b64f54ed4e15d568b5a41e4c9da62c0db25c25
kade <debord253@student.pcsstn.net>
2024-04-22 08:29:16 -0500

    movement

---
+++ script.js
@@ -0,0 +1,32 @@
+const B_Rook_L = document.getElementById("B_Rook_L");
+const B_Rook_R = document.getElementById("B_Rook_R");
+
+const B_Knight_L = document.getElementById("B_Knight_L");
+const B_Knight_R = document.getElementById("B_Knight_R");
+
+const B_Bishop_L = document.getElementById("B_Bishop_L");
+const B_Bishop_R = document.getElementById("B_Bishop_R");
+
+const B_Queen = document.getElementById("B_Queen");
+const B_King = document.getElementById("B_King");
+
+let piece;
+let place;
+document.addEventListener('dragstart',(ev)=>{
+    piece = ev.target;
+    
+});
+
+document.addEventListener('dragenter',(ev)=>{
+    place=ev.target
+    console.log(place)
+    
+})
+document.addEventListener('dragend',(ev)=>{
+    if (place.childElementCount == 0){
+        place.appendChild(piece);
+    }else{
+        console.log("invalid")
+    }
+    
+})
