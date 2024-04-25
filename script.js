e137e813d376e0315e7d84afd6d0bb0a50f68724
Kade <debord253@student.pcsstn.net>
2024-04-25 11:09:59 -0500

    n

--- script.js
+++ script.js
@@ -20,44 +20,89 @@ document.addEventListener('dragend',(ev)=>{
             let newSquare;
             let firstMove;
             square = selection.parentElement.id.split("_")
-            if(square[1] == "2"){
+            let color = selection.id.split("_")
+            if((square[1] == "2" && color[0] == "W") || (square[1] == "7"&& color[0] =="B")){
                 firstMove =true;
             }
-            if(firstMove == true){
-                lenOfSquare = square.length -1;
-                let squareNumber1 = parseInt(square[lenOfSquare]) + 1
-                let newSquare1 = square[0] + "_" + squareNumber1;
-                let squareNumber2 = parseInt(square[lenOfSquare]) + 2
-                let newSquare2 = square[0] + "_" + squareNumber2;
-                if((place.id == newSquare1) ||(place.id == newSquare2) ){
-                        place.appendChild(selection);
-                        console.log("pawn");
-                        firstMove = false;
-                }
-            }else{
-                
-                lenOfSquare = square.length -1;
-                squareNumber = parseInt(square[lenOfSquare]) + 1
-                newSquare = square[0] + "_" + squareNumber;
-                let newASquare1 = ((parseInt(square[0]) +1).toString())+"_"+ squareNumber;
-                let newASquare2 = ((parseInt(square[0]) -1).toString())+"_"+ squareNumber;
-                
-                if(place.id == newSquare ){
-                    if(place.parentElement.childElementCount == 1 && ((place.parentElement.id = newASquare2)||(place.parentElement.id = newASquare1))){
-                        console.log("child")
-                        let Ochild = place.childElement.id.split("_");
-                        let Ychild = selection.id.split("_");
-                        if(Ochild[0] != Ychild[0]){
+            if(color[0]=="W"){
+                if(firstMove == true){
+                    lenOfSquare = square.length -1;
+                    let squareNumber1 = parseInt(square[lenOfSquare]) + 1
+                    let newSquare1 = square[0] + "_" + squareNumber1;
+                    let squareNumber2 = parseInt(square[lenOfSquare]) + 2
+                    let newSquare2 = square[0] + "_" + squareNumber2;
+                    if((place.id == newSquare1) ||(place.id == newSquare2) ){
                             place.appendChild(selection);
                             console.log("pawn");
                             firstMove = false;
+                    }
+                }else{
+                    
+                    lenOfSquare = square.length -1;
+                    squareNumber = parseInt(square[lenOfSquare]) + 1
+                    newSquare = square[0] + "_" + squareNumber;
+                    let newASquare1 = ((parseInt(square[0]) +1).toString())+"_"+ squareNumber;
+                    let newASquare2 = ((parseInt(square[0]) -1).toString())+"_"+ squareNumber;
+                    
+                    if(place.id == newSquare || place.parentElement.id == newSquare){
+                        if(place.parentElement.childElementCount == 1 && ((place.parentElement.id = newASquare2)||(place.parentElement.id = newASquare1))){
+                            console.log("child")
+                            let Ochild = place.id.split("_");
+                            let Ychild = selection.id.split("_");
+                            if(Ochild[0] != Ychild[0]){
+                                place.parentElement.appendChild(selection);
+                                place.parentElement.removeChild(place)
+                                console.log("pawn");
+                                firstMove = false;
+                            }
+                        }else{
+    
+                            console.log("pawn");
+                            place.appendChild(selection);
                         }
-                    }else{
-
-                        console.log("pawn");
-                        place.appendChild(selection);
                     }
                 }
+
+            }
+            if(color[0]=="B"){
+                if(firstMove == true){
+                    lenOfSquare = square.length -1;
+                    let squareNumber1 = parseInt(square[lenOfSquare]) - 1
+                    let newSquare1 = square[0] + "_" + squareNumber1;
+                    let squareNumber2 = parseInt(square[lenOfSquare]) - 2
+                    let newSquare2 = square[0] + "_" + squareNumber2;
+                    if((place.id == newSquare1) ||(place.id == newSquare2) ){
+                            place.appendChild(selection);
+                            console.log("pawn");
+                            firstMove = false;
+                    }
+                }else{
+                    
+                    lenOfSquare = square.length -1;
+                    squareNumber = parseInt(square[lenOfSquare]) - 1
+                    newSquare = square[0] + "_" + squareNumber;
+                    let newASquare1 = ((parseInt(square[0]) +1).toString())+"_"+ squareNumber;
+                    let newASquare2 = ((parseInt(square[0]) -1).toString())+"_"+ squareNumber;
+                    
+                    if(place.id == newSquare || place.parentElement.id == newSquare){
+                        if(place.parentElement.childElementCount == 1 && ((place.parentElement.id = newASquare2)||(place.parentElement.id = newASquare1))){
+                            console.log("child")
+                            let Ochild = place.id.split("_");
+                            let Ychild = selection.id.split("_");
+                            if(Ochild[0] != Ychild[0]){
+                                place.parentElement.appendChild(selection);
+                                place.parentElement.removeChild(place)
+                                console.log("pawn");
+                                firstMove = false;
+                            }
+                        }else{
+    
+                            console.log("pawn");
+                            place.appendChild(selection);
+                        }
+                    }
+                }
+
             }
             
            
@@ -66,7 +111,7 @@ document.addEventListener('dragend',(ev)=>{
             console.log("Rook");
             place.appendChild(selection);
         }
-        if (piece == "Kinght"){
+        if (piece == "Knight"){
             console.log("Kinght");
             place.appendChild(selection);
         }
