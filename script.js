226d1bbfb30ee9f410b02afa41d1f3f8dadd85cc
kade <debord253@student.pcsstn.net>
2024-04-25 09:34:15 -0500

    no

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const lib = ['a','b','c','d','e','f','g','h']
 let piece;
 let place;
 
@@ -11,15 +10,57 @@ document.addEventListener('dragstart',(ev)=>{
 document.addEventListener('dragenter',(ev)=>{
     place=ev.target
     
+    
 })
 document.addEventListener('dragend',(ev)=>{
-    if (place.childElementCount == 0 && (place.className == "White" || place.className == "Black")){
+    if ((place.childElementCount == 0 || place.childElementCount == 1) && (place.className == "White" || place.className == "Black"|| place.parentElement.className == "White" ||place.parentElement.className == "Black")){
         if (piece == "Pawn"){
-            if(place.id == (lib[1]+"3")){
-
-                console.log("pawn");
-                place.appendChild(selection);
+            let lenOfSquare;
+            let squareNumber;
+            let newSquare;
+            let firstMove;
+            square = selection.parentElement.id.split("_")
+            if(square[1] == "2"){
+                firstMove =true;
             }
+            if(firstMove == true){
+                lenOfSquare = square.length -1;
+                let squareNumber1 = parseInt(square[lenOfSquare]) + 1
+                let newSquare1 = square[0] + "_" + squareNumber1;
+                let squareNumber2 = parseInt(square[lenOfSquare]) + 2
+                let newSquare2 = square[0] + "_" + squareNumber2;
+                if((place.id == newSquare1) ||(place.id == newSquare2) ){
+                        place.appendChild(selection);
+                        console.log("pawn");
+                        firstMove = false;
+                }
+            }else{
+                
+                lenOfSquare = square.length -1;
+                squareNumber = parseInt(square[lenOfSquare]) + 1
+                newSquare = square[0] + "_" + squareNumber;
+                let newASquare1 = ((parseInt(square[0]) +1).toString())+"_"+ squareNumber;
+                let newASquare2 = ((parseInt(square[0]) -1).toString())+"_"+ squareNumber;
+                
+                if(place.id == newSquare ){
+                    if(place.parentElement.childElementCount == 1 && ((place.parentElement.id = newASquare2)||(place.parentElement.id = newASquare1))){
+                        console.log("child")
+                        let Ochild = place.childElement.id.split("_");
+                        let Ychild = selection.id.split("_");
+                        if(Ochild[0] != Ychild[0]){
+                            place.appendChild(selection);
+                            console.log("pawn");
+                            firstMove = false;
+                        }
+                    }else{
+
+                        console.log("pawn");
+                        place.appendChild(selection);
+                    }
+                }
+            }
+            
+           
         }
         if (piece == "Rook"){
             console.log("Rook");
